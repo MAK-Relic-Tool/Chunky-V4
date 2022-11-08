1c6c708b300e4ec4b1157a341f0ffa1792252c42
Marcus Kertesz <ModernMAK@Hotmail.com>
2022-11-08 10:31:20 -0800

    Initial commit

---
+++ src/relic/chunky/vX/definitions.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from relic.chunky.core.definitions import Version, ChunkType, ChunkFourCC
+from relic.chunky.core.serialization import MinimalChunkHeader
+
+version = Version(None) # Replace None with the version the plugin is implementing
+
+
+@dataclass
+class ChunkHeader(MinimalChunkHeader):
+    type: ChunkType
+    cc: ChunkFourCC
+    version: int
+    size: int
+    name: str
+    # Add additional chunk header options for the chunky version
+
+
+__all__ = ["version", "ChunkHeader"]
